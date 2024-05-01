8a13e1ea110866effc8780d058713ea9349180da
talhaashraf05 <talha476422@gmail.com>
2024-05-01 14:06:48 +0500

    Added color functionality and image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: [
     "@ant-design-vue/nuxt",
   ],
+  components: true,
 })
